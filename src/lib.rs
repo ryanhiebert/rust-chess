0079a7b688c2e7e6a090aed6ccf08b7bbd79e06b
Ryan Hiebert <ryan@ryanhiebert.com>
2015-03-26 16:55:52 -0500

    Start sketching very simplest parse method

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
+#![feature(plugin)]
+#![plugin(regex_macros)]
+extern crate regex;
+
 pub use color::Color;
 pub use piece::{Piece, Rank};
 pub use ply::{Location, Move, Ply};
 pub use board::{Board, Tile};
 pub use game::Game;
+pub use parser::{Notation, FromToZeroIntegers};
 
 mod color;
 mod piece;
 mod ply;
 mod board;
 mod game;
+mod parser;
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use ply::Ply;
+use ply::{Ply, Location, Move};
 use board::Board;
 
 
@@ -7,11 +7,46 @@ pub trait Notation {
 }
 
 
-pub struct StandardAlgebraicNotation;
-pub struct FigurineAlgebraicNotation;
-pub struct LongAlgebraicNotation;
+// pub struct StandardAlgebraicNotation;
+// pub struct FigurineAlgebraicNotation;
+// pub struct LongAlgebraicNotation;
 
 
-/// Simple algebraic notation for noting from and to moves.
-/// Only basic moves supported.
-pub struct FromToAlgebraicNotation;
+/// Simplest input method to parse.
+/// Takes pairs of zero based integers to form the move.
+/// Captures are determined automatically.
+/// Special moves are not allowed, including promotions.
+///
+/// Examples:
+///
+/// 00 01  // a1 a2
+/// 77 76  // h8 h7
+pub struct FromToZeroIntegers;
+
+impl Notation for FromToZeroIntegers {
+    fn parse(&self, _: &Board, _: &str) -> Option<Ply> {
+        //let re = regex!(r"^(\d{2}) *(\d{2})");
+        Some(Ply::Basic(Move {
+            from: Location { file: 0, rank: 0 },
+            to: Location { file: 0, rank: 1 },
+        }, None))
+    }
+}
+
+
+#[cfg(tests)]
+mod tests {
+    use board::Board;
+    use super::FromToZeroIntegers;
+
+    #[test]
+    fn parse() {
+        let notation = FromToZeroIntegers;
+        let ply = notation.parse(&Board::new(), "00 01");
+        let expected = Option(Ply::Basic(Move {
+            from: Location { file: 0, rank: 0 },
+            to: Location { file: 0, rank: 1 },
+         }, None));
+        assert!(ply == expected);
+    }
+}
